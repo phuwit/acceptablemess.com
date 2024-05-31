3516bc60893fff0528586d1dace04864147ae54c
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-05-31 21:20:54 +0700

    updated rotate options

--- src/lib/components/scrabble/scrabble-tile.svelte
+++ src/lib/components/scrabble/scrabble-tile.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
   import { getScrabbleScore } from './scrabble-scores';
   import { cn } from '$lib/utils';
+  import type { ClassValue } from 'clsx';
 
   export let character: string;
 
-  function getRandomRotation() {
-    const rotations = ['rotate-1', 'rotate-2', 'rotate-3', '-rotate-1', '-rotate-2', '-rotate-3']
+  function getRandomRotation(): ClassValue {
+    const rotations = ['rotate-2', 'rotate-3', 'rotate-0', '-rotate-2', '-rotate-3'];
     return rotations[Math.floor(Math.random() * rotations.length)];
   }
 </script>
 
 <div
-  class={
-    cn(
-      'relative h-20 w-20 items-center justify-center rounded-md bg-yellow-100 text-4xl text-black transition-transform hover:scale-125 drop-shadow-md',
-      getRandomRotation(),
-      `hover:${getRandomRotation()}`
-    )
-  }
+  class={cn(
+    'relative h-20 w-20 items-center justify-center rounded-md bg-yellow-100 text-4xl text-black drop-shadow-md transition-transform hover:scale-125',
+    getRandomRotation(),
+    `hover:${getRandomRotation()}`
+  )}
 >
   <div class="absolute flex h-full w-full items-center justify-center text-3xl">
     {character}

142b4ba8b8ac00191670195c83ca6e27b4268ea6
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-06-10 19:33:32 +0700

    update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,25 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter()
+    adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		}),
+    prerender: {
+      handleMissingId: (id, path) => {
+        console.warn(`Missing element with id "${id}" on page "${path}"`);
+        return 'ignore'; // or 'remove' or 'fail'
+      }
+    }
   }
 };
 
